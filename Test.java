94eeb401663505f8a71f8d3633816f6e37bb6973
Hyn2 <125263770+Hyn2@users.noreply.github.com>
2023-03-13 18:58:23 +0900

    Delete Test.java

--- Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-        public static void main(String[]args) {
-            Typecasting tc= new Typecasting();
-            tc.setName("HongGilDong");
-            System.out.println(tc.getName());
-        }
-}
