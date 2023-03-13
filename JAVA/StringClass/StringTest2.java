6c79007174d78c1eeebc6825a63ea1842f0b57e1
Hyn2 <125263770+Hyn2@users.noreply.github.com>
2023-03-13 19:11:37 +0900

    230313

---
+++ JAVA/StringClass/StringTest2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+public class StringTest2 {
+    public static void main(string[] args) {
+        String str;
+        Scanner scn = new Scanner(System.in);
+
+
+        while (true) {
+            System.out.print("문자열을 입력하세요 : ");
+            str = sc.next();
+            if(str.equals("quit") == true) {
+                break;
+            }
+            if (str.matches("^www.\\.(.+)")) {
+                System.out.println(str + " 은 'www'로 시작합니다.");
+            } else {
+                System.out.println(str + " 은 'www'로 시작하지 않습니다.");
+            }
+        }
+    }
+}
